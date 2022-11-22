b9eeace232bdaa9e30dc3ce4c102d68f5cec2983
ToroEtele <toroetele@gmail.com>
2022-11-22 21:52:07 +0200

    Work on Factory Functions

---
+++ Classes and Prototypes/2-factory-functions-new.js
@@ -0,0 +1,11 @@
+function userCreator(name, score) {
+  this.name = name;
+  this.score = scroe;
+}
+
+userCreator.prototype
+userCreator.prototype.increment = function() {
+  this.score++;
+}
+
+const user = new userCreator("Will", 3);
